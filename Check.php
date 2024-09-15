db099101c2de12c7586e5bb714addc6c1b681938
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-15 00:05:43 -0400

    Update Check.php

---
+++ Check.php
@@ -0,0 +1,77 @@
+<?php
+$errors = [];
+$firstName = $lastName = $dob = $gender = $email = $cellNumber = $batch = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = sanitize_input($_POST['first_name']);
+    $lastName = sanitize_input($_POST['last_name']);
+    $dob = sanitize_input($_POST['dob']);
+    $gender = sanitize_input($_POST['gender']);
+    $email = sanitize_input($_POST['email']);
+    $cellNumber = sanitize_input($_POST['cell_number']);
+    $batch = sanitize_input($_POST['batch']);
+
+    validate_empty('first_name', $firstName);
+    validate_empty('last_name', $lastName);
+    validate_empty('dob', $dob);
+    validate_empty('gender', $gender);
+    validate_empty('email', $email);
+    validate_empty('cell_number', $cellNumber);
+    validate_empty('batch', $batch);
+    
+    validate_no_special_chars('first_name', $firstName);
+    validate_no_special_chars('last_name', $lastName);
+    
+    validate_email($email);
+    validate_cell_number($cellNumber);
+    validate_age($dob);
+    
+    if (empty($errors)) {
+        header('Location: success.php');
+        exit();
+    }
+}
+
+function sanitize_input($data) {
+    return htmlspecialchars(trim($data));
+}
+
+function validate_empty($field, $value) {
+    global $errors;
+    if (empty($value)) {
+        $errors[$field] = ucfirst(str_replace('_', ' ', $field)) . " is required.";
+    }
+}
+
+function validate_no_special_chars($field, $value) {
+    global $errors;
+    if (!preg_match("/^[a-zA-Z ]*$/", $value)) {
+        $errors[$field] = ucfirst(str_replace('_', ' ', $field)) . " should not contain special characters.";
+    }
+}
+
+function validate_email($email) {
+    global $errors;
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = "Invalid email format.";
+    }
+}
+
+function validate_cell_number($cellNumber) {
+    global $errors;
+    if (!preg_match("/^[0-9]{10}$/", $cellNumber)) {
+        $errors['cell_number'] = "Invalid cell number. Must be 10 digits.";
+    }
+}
+
+function validate_age($dob) {
+    global $errors;
+    $birthDate = new DateTime($dob);
+    $currentDate = new DateTime();
+    $age = $currentDate->diff($birthDate)->y;
+
+    if ($age < 18) {
+        $errors['dob'] = "You must be 18+ to register.";
+    }
+}
+?>
